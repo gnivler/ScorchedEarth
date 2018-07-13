7f00b91668daef6a8b7e0f96c7c04b4d18e27646
gnivler <gnivil@gmail.com>
2018-07-13 10:30:58 -0600

    working, finally fucking working

--- ScorchedEarth/ScorchedEarth/ScorchedEarth.cs
+++ ScorchedEarth/ScorchedEarth/ScorchedEarth.cs
@@ -1,24 +1,77 @@
-﻿using Harmony;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
-using Newtonsoft.Json;
+using System.Reflection.Emit;
+using System.Text;
+using BattleTech.Rendering;
+using Harmony;
 
 namespace ScorchedEarth
 {
-    public class Settings
-    {
-        public bool Debug = false;
-    }
-
     public class ScorchedEarth
     {
-        internal static string ModDirectory;
-        internal static Settings settings;
-        public static void Init(string directory, string settingsJSON)
+        public StringBuilder Builder = new StringBuilder();
+
+        public static void Init(string directory, string settingsJson)
         {
             var harmony = HarmonyInstance.Create("ca.gnivler.ScorchedEarth");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
-            settings = JsonConvert.DeserializeObject<Settings>(settingsJSON);
-            ModDirectory = directory;
         }
+
+        // strategy:  intercept calls to realtimeSinceStartup to make things practically infinite
+        //            intercept calls for the only int32 on TerrainDecal (MaxDecals) and make it a high value
+        [HarmonyPatch(typeof(FootstepManager), "ProcessFootsteps")]
+        public static class PatchProcessFootsteps
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                for (var i = 0; i < codes.Count(); i++)
+                {
+
+                    if (codes[i].operand != null && codes[i].operand.ToString().Contains("get_realtimeSinceStartup"))
+                    {
+                        codes[i].opcode = OpCodes.Ldc_R4;
+                        codes[i].operand = float.MinValue;
+                    }
+
+                    if (codes[i].operand != null && codes[i].operand.ToString()
+                            .Contains("FootstepManager/TerrainDecal>::get_Item(int32)"))
+                    {
+                        codes[i].opcode = OpCodes.Ldc_I4_4;
+                        codes[i].operand = int.MaxValue;
+                    }
+                }
+
+                return codes.AsEnumerable();
+            }
+        }
+
+        [HarmonyPatch(typeof(FootstepManager), "ProcessScorches")]
+        public static class PatchProcessScorches
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                for (var i = 0; i < codes.Count(); i++)
+                {
+                    if (codes[i].operand != null && codes[i].operand.ToString().Contains("get_realtimeSinceStartup"))
+                    {
+                        codes[i].opcode = OpCodes.Ldc_R4;
+                        codes[i].operand = float.MinValue;
+                    }
+
+                    if (codes[i].operand != null && codes[i].operand.ToString()
+                            .Contains("FootstepManager/TerrainDecal>::get_Item(int32)"))
+                    {
+                        codes[i].opcode = OpCodes.Ldc_I4_4;
+                        codes[i].operand = int.MaxValue;
+                    }
+                }
+
+                return codes.AsEnumerable();
+            }
+        }
+
     }
 }
